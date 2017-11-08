16d924780fb0518d6115f5c7bc0de5a3bcfe233b
starekp <pavol.starek@launcher.sk>
2017-11-08 12:48:22 +0100

    next

--- vars/phabSendLint.groovy
+++ vars/phabSendLint.groovy
@@ -1,12 +1,11 @@
-def call(body) {
+def call(String filename, Boolean debug = false, String pwd , body) {
 	def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-	def debug = config.debug ?: false
-	def wd = config.pwd ? new File(config.pwd) : new File('.')
-	def file = new File(wd, config.filename)
+	def wd = pwd ? new File(pwd) : new File('.')
+	def file = new File(wd, filename)
 
 	echo "File to parse ${file.absolutePath}, WD ${wd.absolutePath}"
 
