2a0cf1e7f72048d3b28466f7ff79e7477ad570b5
starekp <pavol.starek@launcher.sk>
2017-11-08 13:57:37 +0100

    next

--- vars/phabSendLint.groovy
+++ vars/phabSendLint.groovy
@@ -1,13 +1,13 @@
 def call(String filename, Boolean debug = false, Closure body = null) {
 	def config = [:]
 
+	try {
 	if (body) {
     	body.resolveStrategy = Closure.DELEGATE_FIRST
 	    body.delegate = config
     	body()
 	}
 
-	try {
 	def f = new File(filename)
 	def wd = new File(pwd())
 
