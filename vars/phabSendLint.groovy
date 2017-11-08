df2e4b657d8c2e0274ff6d3eeca0d248aef73911
starekp <pavol.starek@launcher.sk>
2017-11-08 14:32:08 +0100

    next

--- vars/phabSendLint.groovy
+++ vars/phabSendLint.groovy
@@ -1,8 +1,12 @@
-def call(Boolean debug = false, String filename) {
+def call(String filename, Map options = [:]) {
+	def opt = [
+		debug: false
+	] + options
+
 	def f = new File(filename)
 	def wd = new File(pwd())
 
 	f = f.absolute ?: new File(wd, filename)
 
-	echo "${f.absolutePath}"
+	!opt.debug ?: echo "${f.absolutePath}"
 }
