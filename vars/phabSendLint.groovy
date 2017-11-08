0678d9fa0a8ec67908e20a277b1db732f53ab628
starekp <pavol.starek@launcher.sk>
2017-11-08 13:55:05 +0100

    next

--- vars/phabSendLint.groovy
+++ vars/phabSendLint.groovy
@@ -4,14 +4,9 @@ def call(String filename, Boolean debug = false, Closure body = null) {
     body.delegate = config
     body()
 
-	!debug ?: echo "Filename: ${filename}"
 	def f = new File(filename)
 	def wd = new File(pwd())
 
-	!debug ?: echo "${WD: ${wd.absolutePath}"
 	f = f.absolute ?: new File(wd, filename)
-
-	!debug ?: echo "${Final: ${f.absolutePath}"
-	echo "File to parse ${file.absolutePath}, WD ${wd.absolutePath}"
 */
 }
